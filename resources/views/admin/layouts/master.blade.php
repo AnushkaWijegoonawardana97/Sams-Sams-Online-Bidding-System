e14953c5500f68d5d5ba0b8ea5e372457edb29b3
Anushka Wijegoonawardana <anushkaduwolka123@gmail.com>
2021-08-04 21:25:07 +0530

    auth routes & fixes

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -210,14 +210,14 @@
                         </ul>
                     </li>
 
-                    <li class="nav-item">
-                        <a href="{{ route('product_category.index') }}" class="nav-link {{ Request::is('admin/product-category*') ? 'active' : '' }}">
-                            <i class="nav-icon fas fa-th"></i>
-                            <p>Product Category</p>
-                        </a>
-                    </li>
-
-
+                    @role('super_admin|seller')
+                        <li class="nav-item">
+                            <a href="{{ route('product_category.index') }}" class="nav-link {{ Request::is('admin/product-category*') ? 'active' : '' }}">
+                                <i class="nav-icon fas fa-th"></i>
+                                <p>Product Category</p>
+                            </a>
+                        </li>
+                    @endrole
                 </ul>
             </nav>
             <!-- /.sidebar-menu -->
