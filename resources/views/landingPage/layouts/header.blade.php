6c8dc8b175373d555fe738318dbae744037e27e4
Anushka Wijegoonawardana <anushkaduwolka123@gmail.com>
2021-08-07 21:00:10 +0530

    frontend ui development phase

--- resources/views/landingPage/layouts/header.blade.php
+++ resources/views/landingPage/layouts/header.blade.php
@@ -1,3 +1,34 @@
-<section>
-    header works !
-</section>
+<header class="header-navigation navbar navbar-expand-lg" id="page-header-navigation">
+    <!-- Top Header -->
+    <nav class="top-header-nav d-flex align-items-center">
+        <!-- Navbar Branding -->
+        <a href="" class="navbar-brand">
+            <img width="175" height="50" src="{{asset('img/Logo.png')}}" alt="">
+        </a>
+
+        <!-- Navbar Links -->
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About Us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Shop</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Browse Items</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+            </li>
+        </ul>
+
+        <!-- Navbar Buttons -->
+        <a href="" class="btn btn-theme-secondary">Login</a>
+    </nav>
+
+    <!-- Bottom Header -->
+    <div class="bottom-header-nav"></div>
+</header>
--- resources/views/landingPage/layouts/master.blade.php
+++ resources/views/landingPage/layouts/master.blade.php
@@ -14,5 +14,7 @@
     @yield('content')
 
     @include('landingPage.layouts.footer')
+
+    <script src="{{asset('js/main.js')}}"></script>
 </body>
 </html>
