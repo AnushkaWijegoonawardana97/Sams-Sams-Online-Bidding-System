6e501df68492e20850674af6a26ae8e7dc543747
Anushka Wijegoonawardana <anushkaduwolka123@gmail.com>
2021-08-08 13:03:21 +0530

    Footer development completed

--- resources/views/landingPage/layouts/footer.blade.php
+++ resources/views/landingPage/layouts/footer.blade.php
@@ -1,3 +1,81 @@
-<section>
-    footer works !
-</section>
+<footer class="page-footer" id="page-footer">
+    <!-- Footer Section Top -->
+    <section class="footer-top-section">
+        <!-- Footer Top Section Colums -->
+        <div class="footer-column">
+            <div class="footer-about">
+                <a href="#" class="footer-about-branding">
+                    <img width="175" height="50" src="{{asset('img/Logo.png')}}" alt="">
+                </a>
+
+                <div class="foooter-about-text">
+                    <P>Street addresses: 445 Mount</P> 
+                    <P>Eden Road, Mount Eden,</P> 
+                    <P>Auckland - Postcodes: 5022</P> 
+                </div>
+            </div>
+
+            <div class="footer-social">
+                <div class="footer-social-header">
+                    FOLLOW US
+                </div>
+
+                <div class="footer-social-links">
+                    <a href="" class="footer-social-link">
+                        <i class="fab fa-facebook"></i>
+                    </a>
+                    <a href="" class="footer-social-link">
+                        <i class="fab fa-facebook"></i>
+                    </a>
+                    <a href="" class="footer-social-link">
+                        <i class="fab fa-facebook"></i>
+                    </a>
+                    <a href="" class="footer-social-link">
+                        <i class="fab fa-facebook"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <!-- Footer Top Section Colums -->
+        <div class="footer-column">
+            <div class="footer-column-header">COMPANY</div>
+
+            <div class="footer-column-links d-flex flex-column">
+                <a href="" class="footer-link-item">Profile</a>
+                <a href="" class="footer-link-item">Profile</a>
+                <a href="" class="footer-link-item">Profile</a>
+                <a href="" class="footer-link-item">Profile</a>
+            </div>
+        </div>
+
+        <!-- Footer Top Section Colums -->
+        <div class="footer-column">
+            <div class="footer-column-header">COMPANY</div>
+
+            <div class="footer-column-links d-flex flex-column">
+                <a href="" class="footer-link-item">Profile</a>
+                <a href="" class="footer-link-item">Profile</a>
+                <a href="" class="footer-link-item">Profile</a>
+                <a href="" class="footer-link-item">Profile</a>
+            </div>
+        </div>
+
+        <!-- Footer Top Section Colums -->
+        <div class="footer-column">
+            <div class="footer-column-header">COMPANY</div>
+
+            <div class="footer-column-links d-flex flex-column">
+                <a href="" class="footer-link-item">Profile</a>
+                <a href="" class="footer-link-item">Profile</a>
+                <a href="" class="footer-link-item">Profile</a>
+                <a href="" class="footer-link-item">Profile</a>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer Section Bottom -->
+    <section class="footer-bottom-section text-center">
+        <p>Copyright © 2019, Template</p>
+    </section>
+</footer>
