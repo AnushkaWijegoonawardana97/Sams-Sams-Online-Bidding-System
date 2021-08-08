7d8ff51780c1ea3500afbcf9bfe3f804e8a2d161
Anushka Wijegoonawardana <anushkaduwolka123@gmail.com>
2021-08-08 11:28:30 +0530

    frontend ui header completed

--- resources/views/landingPage/layouts/header.blade.php
+++ resources/views/landingPage/layouts/header.blade.php
@@ -1,3 +1,47 @@
+<!-- Mobile Overlayer Navigation -->
+<section class="mobile-overlayer-navigation d-flex align-items-center justify-content-center" id="mobile-ovelayer-navigation">
+    <!-- Mobile Close Nav Button -->
+    <a href="#" class="mobile-closenav-button" id="mobile-closenav-button"><i class="fas fa-times"></i></a>
+
+    <!-- Overlayer Navlink Content -->
+    <ul class="overlayer-navcontent">
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">About Us</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Shop</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Browse Items</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+        </li>
+
+        <!-- Cart Icons -->
+        <li class="navbar-icon-group d-flex">
+            <a href="" class="icon-wishlist icon-item">
+                <span class="icon"><i class="fas fa-heart"></i></span>
+                <span class="badge badge-pill">0</span>
+            </a>
+
+            <a href="" class="icon-cart icon-item">
+                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                <span class="badge badge-pill">10</span>
+            </a>
+
+            <div class="cart-amount d-flex">
+                <span class="cart-text">Your cart</span>
+                <span class="cart-price">$0.00</span>
+            </div>
+        </li>
+    </ul>
+</section>
+
+<!-- Header Navigation -->
 <header class="header-navigation navbar navbar-expand-lg" id="page-header-navigation">
     <!-- Top Header -->
     <nav class="top-header-nav d-flex align-items-center">
@@ -7,7 +51,7 @@
         </a>
 
         <!-- Navbar Links -->
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0 desktop-only">
             <li class="nav-item active">
                 <a class="nav-link" href="#">Home</a>
             </li>
@@ -26,9 +70,50 @@
         </ul>
 
         <!-- Navbar Buttons -->
-        <a href="" class="btn btn-theme-secondary">Login</a>
+        <a href="" class="btn btn-theme-secondary desktop-only">Login</a>
+
+        <!-- Mobile Open Nav Button -->
+        <a href="#" class="mobile-opennav-button mobile-only" id="mobile-opennav-button"><i class="fas fa-bars"></i></a>
     </nav>
 
     <!-- Bottom Header -->
-    <div class="bottom-header-nav"></div>
+    <div class="bottom-header-nav d-flex align-items-center justify-content-between">
+        <!-- Navbar Category Button -->
+        <div class="dropdown">
+            <button class="btn btn-theme-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                Categories
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">Electronic</a>
+                <a class="dropdown-item" href="#">Vehicles</a>
+                <a class="dropdown-item" href="#">Services</a>
+            </div>
+        </div>
+
+        <!-- Search Bar -->
+        <div class="input-group search-bar">
+            <input type="text" class="form-control" placeholder="Search Products" aria-label="Search Products" aria-describedby="search-icon">
+            <div class="input-group-append">
+                <span class="input-group-text" id="search-icon"><i class="fas fa-search"></i></span>
+            </div>
+        </div>
+
+        <!-- Cart Icons -->
+        <div class="navbar-icon-group d-flex">
+            <a href="" class="icon-wishlist icon-item">
+                <span class="icon"><i class="fas fa-heart"></i></span>
+                <span class="badge badge-pill">0</span>
+            </a>
+
+            <a href="" class="icon-cart icon-item">
+                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                <span class="badge badge-pill">10</span>
+            </a>
+
+            <div class="cart-amount d-flex">
+                <span class="cart-text">Your cart</span>
+                <span class="cart-price">$0.00</span>
+            </div>
+        </div>
+    </div>
 </header>
