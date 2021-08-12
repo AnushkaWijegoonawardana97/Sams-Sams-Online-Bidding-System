d437704c2f85603ffcc72b5da4f09237b7b7090d
Anushka Wijegoonawardana <anushkaduwolka123@gmail.com>
2021-08-12 07:54:01 +0530

    Registration main page completed

--- resources/views/authPage/register.blade.php
+++ resources/views/authPage/register.blade.php
@@ -16,34 +16,39 @@
 
             <!-- Auth Form Header -->
             <Header class="auth-form-header">
-                <h1 class="auth-header-title">Welcome Back.</h1>
-                <p class="auth-header-test">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, rem?</p>
+                <h1 class="auth-header-title">Select Account Type.</h1>
+                <p class="auth-header-test">Please select the type of Account You want to Sign Up for:</p>
             </Header>
 
             <!-- Register Buttons -->
             <div class="div register-button-container d-flex align-items-center justify-content-between">
                 <!-- Seller Registration -->
-                <a href="{{ route('auth.sellerRegistration') }}" class="register-btn" id="seller-registration">
-                    <div class="register-btn-icon">
-                        <img src="" alt="">
-                    </div>
-
-                    <div class="register-btn-text">
-                        Register as a Seller
-                    </div>
-                </a>
+                <div class="registration-btn" id="seller-registration">
+                    <a href="{{ route('auth.sellerRegistration') }}" class="register-btn-link d-block">
+                        <img src="{{asset('img/user-tag-solid.svg')}}" alt="" class="registration-icon" width="200" height="200">
+                        <a href="{{ route('auth.sellerRegistration') }}" class="btn btn-block btn-outline-primary registration-text-btn">Seller Account</a>
+                    </a>
+                </div>
 
                 <!-- Buyer Registration -->
-                <a href="{{ route('auth.buyerRegistration') }}" class="register-btn" id="seller-registration">
-                    <div class="register-btn-icon">
-                        <img src="" alt="">
-                    </div>
-
-                    <div class="register-btn-text">
-                        Register as a Buyer
-                    </div>
-                </a>
+                <div class="registration-btn" id="buyer-registration">
+                    <a href="{{ route('auth.buyerRegistration') }}" class="register-btn-link d-block">
+                        <img src="{{asset('img/user-tag-solid.svg')}}" alt="" class="registration-icon" width="200" height="200">
+                        <a href="{{ route('auth.buyerRegistration') }}" class="btn btn-block btn-outline-primary registration-text-btn">Buyer Account</a>
+                    </a>
+                </div>
             </div>
+
+            <!-- Auth Footer  -->
+            <footer class="auth-footer text-center">
+                <p class="auth-footer-text">
+                    Go back to <a href="{{ route('auth.login') }}" class="login-link">Login</a>
+                </p>
+
+                <p class="auth-footer-text">
+                    By clicking on sign up button above, you confirm that you agree to the Skooleeo <a href="" class="login-link">Terms of Service</a> and <a href="" class="login-link">Privacy Policy</a>
+                </p>
+            </footer>
         </div>
     </section>
 @endsection
