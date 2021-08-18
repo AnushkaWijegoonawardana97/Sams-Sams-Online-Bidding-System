cd1e5c5e394e7369f37efc3d5192c9911445eae5
Anushka Wijegoonawardana <anushkaduwolka123@gmail.com>
2021-08-18 20:17:23 +0530

    login flow development

--- resources/views/authPage/sellerRegistration.blade.php
+++ resources/views/authPage/sellerRegistration.blade.php
@@ -1,1 +1,36 @@
-<h1>Seller Registration</h1>
+@extends('authPage.layouts.master')
+
+@section('content')
+    <!-- Register Page Banner Container -->
+    <section class="register-form-banner" id="seller-form-banner"></section>
+
+    <!-- Register Page Form Container -->
+    <section class="register-form-container" id="seller-form-container">
+        <div class="register-form-inner">
+            <!-- Register Form Navigation -->
+            <nav class="register-form-nav">
+                <a href="/" class="register-barnding">
+                    <img width="175" height="50" src="{{asset('img/Logo.png')}}" alt="" class="register-logo">
+                </a>
+            </nav>
+
+            <!-- Registration Wizzard -->
+            <div class="registration-wizard-container">
+                <div class="registration-wizard-header"></div>
+
+                <form action="" class="registration-form">
+                    <div class="form-wizard-card" id="step">
+                        
+                    </div>
+                </form>
+            </div>
+
+            <!-- Register Footer  -->
+            <footer class="register-footer text-center">
+                <p class="register-footer-text">
+                    Don't have an account? <a href="{{ route('auth.register') }}" class="login-link">Register</a>
+                </p>
+            </footer>
+        </div>
+    </section>
+@endsection
