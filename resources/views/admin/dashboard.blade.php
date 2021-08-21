6a6b1f4bc10c4df808f558258c6903235ff3dcef
Anushka Wijegoonawardana <anushkaduwolka123@gmail.com>
2021-08-21 09:14:00 +0530

    dashboard activity log section created

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,13 +8,12 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Starter Page</h1>
+                    <h1 class="m-0">Dashboard</h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Starter Page</li>
-                    </ol>
+                    <!-- <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item">Dashboard</li>
+                    </ol> -->
                 </div><!-- /.col -->
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
@@ -25,7 +24,7 @@
     <div class="content">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-6">
+                <div class="col-lg-8">
                     <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
@@ -53,36 +52,53 @@
                         </div>
                     </div><!-- /.card -->
                 </div>
-                <!-- /.col-md-6 -->
-                <div class="col-lg-6">
+                
+                <div class="col-lg-4">
                     <div class="card">
                         <div class="card-header">
-                            <h5 class="m-0">Featured</h5>
+                            <div class="d-flex align-items-center justify-content-between">
+                                <h3 class="card-title mb-0 font-weight-bold">
+                                    <i class="fas fa-clipboard-list"></i>
+                                    Activity Log
+                                </h3>
+
+                                <a href="#" class="card-link font-weight-bold">View More</a>
+                            </div>
                         </div>
                         <div class="card-body">
-                            <h6 class="card-title">Special title treatment</h6>
+                            <!-- Activity Item -->
+                            <div class="card card-primary card-outline">
+                                <div class="card-body">
+                                    <div class="font-weight-bold">Adam Smith new buyer has registerd to the system</div>
+                                    <div class="d-flex align-items-center justify-content-between">
+                                        <div>
+                                            <span class="badge badge-primary">Buyer Registration</span>
+                                            <span class="badge badge-primary">Online Registration</span>
+                                        </div>
+                                        <div class="font-italic"><i class="far fa-clock"></i> 3 minutes ago</div>
+                                    </div>
+                                </div>
+                            </div>
 
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-
-                    <div class="card card-primary card-outline">
-                        <div class="card-header">
-                            <h5 class="m-0">Featured</h5>
-                        </div>
-                        <div class="card-body">
-                            <h6 class="card-title">Special title treatment</h6>
-
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <!-- Activity Item -->
+                            <div class="card card-danger card-outline">
+                                <div class="card-body">
+                                    <div class="font-weight-bold">Adam Smith has cancled the bid #345</div>
+                                    <div class="d-flex align-items-center justify-content-between">
+                                        <div>
+                                            <span class="badge badge-warning">Biding Cancelation</span>
+                                            <span class="badge badge-danger">User Cancelation</span>
+                                        </div>
+                                        <div class="font-italic"><i class="far fa-clock"></i> 3 minutes ago</div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <!-- /.col-md-6 -->
+
             </div>
-            <!-- /.row -->
-        </div><!-- /.container-fluid -->
+            
+        </div>
     </div>
-    <!-- /.content -->
 @endsection
