1cff597bfa8c51da63d3cb1a2dcb4ad579a6c224
Anushka Wijegoonawardana <anushkaduwolka123@gmail.com>
2021-08-22 09:42:16 +0530

    fixes

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,52 @@
-@extends('layouts.app')
+@extends('auth.layouts.master')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+    <!-- Auth Page Banner Container -->
+    <section class="auth-form-banner" id="fgpw-form-banner"></section>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <!-- Auth Page Form Container -->
+    <section class="auth-form-container" id="fgpw-form-container">
+        <div class="auth-form-inner">
+            <!-- Auth Form Navigation -->
+            <nav class="auth-form-nav">
+                <a href="/" class="auth-barnding">
+                    <img width="175" height="50" src="{{asset('img/Logo.png')}}" alt="" class="auth-logo">
+                </a>
+            </nav>
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
+            <!-- Auth Form Header -->
+            <Header class="auth-form-header">
+                <h1 class="auth-header-subtitle">Recover your account.</h1>
+                <p class="auth-header-test">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, rem?</p>
+            </Header>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+            <!-- fgpw Form -->
+            <form method="POST" action="{{ route('password.email') }}">
+                @csrf
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                <div class="form-group mb-4">
+                    <label for="usernameemail">Email address</label>
+                    <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email" autofocus>
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                 </div>
-            </div>
+                <button type="submit" class="btn btn-primary btn-block">Send Password Reset Link</button>
+            </form>
+
+            <!-- Auth Footer  -->
+            <footer class="auth-footer text-center">
+                <p class="auth-footer-text">
+                    Back to the <a href="{{ route('login') }}" class="login-link">Login</a>
+                </p>
+            </footer>
         </div>
-    </div>
-</div>
+    </section>
 @endsection
