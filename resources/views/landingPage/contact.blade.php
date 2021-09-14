50b3dab6063c0f82ab3693b57d3180f9bd33b592
Anushka Wijegoonawardana <anushkaduwolka123@gmail.com>
2021-09-14 07:02:41 +0530

    contact page fixes

--- resources/views/landingPage/contact.blade.php
+++ resources/views/landingPage/contact.blade.php
@@ -5,7 +5,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-12 m-auto">
-                <div style="width: 100%"><iframe width="100%" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=300&amp;hl=en&amp;q=colombo%20+(Sams%20&amp;%20Sams)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe></div>
+                <div style="width: 100%"><iframe width="100%" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=500&amp;hl=en&amp;q=colombo%20+(Sams%20&amp;%20Sams)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe></div>
                 </div>
             </div>
          </div>
